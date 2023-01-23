5b76ff0bd384dcfebfaada653130507216ad0155
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-23 20:32:20 +0300

    SpringBootApplication ==> Security (exclude)

--- src/main/java/com/muhammetcakir/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/muhammetcakir/KodluyoruzSpringBootApplication.java
@@ -3,7 +3,11 @@ package com.muhammetcakir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class KodluyoruzSpringBootApplication {
 
 	public static void main(String[] args) {
