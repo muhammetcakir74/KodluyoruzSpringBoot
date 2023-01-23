aaf06b2223e43506c7ee4c86b5637f6ffa4ae413
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-23 22:52:55 +0300

    Bean yapısı

---
+++ src/main/java/com/muhammetcakir/controller/BeanController.java
@@ -0,0 +1,24 @@
+package com.muhammetcakir.controller;
+
+import com.muhammetcakir.bean.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class BeanController {
+
+    @Autowired
+    BeanConfig beanConfig;
+
+
+    // http://localhost:8080/bean/beanDto
+    @GetMapping("/bean/beanDto") //url yapısı
+    @ResponseBody //html olmadan direk ekranda göster
+    public String getBeanDto(){
+
+        return beanConfig.beanDto()+".";
+    }
+}
