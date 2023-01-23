8a0ce5b45c3d08d3ef7c0964744cb5301ff62e02
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-23 20:33:02 +0300

    DevTools'u Aktif etmek

---
+++ src/main/java/com/muhammetcakir/utils/ConfigProgramming.java
@@ -0,0 +1,9 @@
+package com.muhammetcakir.utils;
+
+public class ConfigProgramming {
+
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically(OK Clicked) (Ctrl+Alt+S)
+    //file ==> Settings ==> Advanced Settings ==> Allow Auto-make to start even (OK clicked)
+
+}
