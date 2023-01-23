649901a4af0b2b312eef771e4e8331480bcca27f
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-23 20:32:35 +0300

    Kısayollar

---
+++ src/main/java/com/muhammetcakir/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.muhammetcakir.utils;
+
+public class IntellijShortCut {
+    /*Setting ==> ctrl+alt+s
+    Alt+Insert ==> constructor,toString,getter,setter
+    Ctrl+/ ==> single comment
+    Ctrl+shift+/ ==> multiple comment*/
+}
