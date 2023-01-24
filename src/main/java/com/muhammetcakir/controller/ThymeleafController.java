bff8debe86258cefad5e3dba706c46222b149c66
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-24 21:51:31 +0300

    Thymeleaf

---
+++ src/main/java/com/muhammetcakir/controller/ThymeleafController.java
@@ -0,0 +1,16 @@
+package com.muhammetcakir.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ThymeleafController {
+
+    //http://localhost:8080/thymeleaf1
+    @GetMapping("/thymeleaf1")
+    //@ResponseBody
+    public String getThymeleaf1(){
+        return "thymeleaf1";
+    }
+}
