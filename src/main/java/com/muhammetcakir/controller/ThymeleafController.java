801e84bf48b6af61791382fecd5769140c68e525
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-25 12:13:42 +0300

    Thymeleaf css,html,model

--- src/main/java/com/muhammetcakir/controller/ThymeleafController.java
+++ src/main/java/com/muhammetcakir/controller/ThymeleafController.java
@@ -1,6 +1,7 @@
 package com.muhammetcakir.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,4 +14,20 @@ public class ThymeleafController {
     public String getThymeleaf1(){
         return "thymeleaf1";
     }
+    //http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model(Model model){
+        model.addAttribute("key_model1", "Ben modelden geldim1");
+        model.addAttribute("key_model2", "Ben modelden geldim2");
+        return "thymeleaf1";
+    }
+
+    //http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf3Model(Model model){
+        model.addAttribute("key_model1", "Ben modelden geldim1");
+        model.addAttribute("key_model2", "Ben modelden geldim2");
+        return "thymeleaf_file/thymeleaf3";
+    }
+
 }
