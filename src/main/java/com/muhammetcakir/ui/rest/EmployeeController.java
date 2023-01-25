35b0237b559c87cd7b42b80eb0e4e2336e8f0005
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-26 00:42:23 +0300

    EmployeeController

---
+++ src/main/java/com/muhammetcakir/ui/rest/EmployeeController.java
@@ -0,0 +1,9 @@
+package com.muhammetcakir.ui.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+}
