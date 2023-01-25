adef4b819819af825b5b616b5ac9754fac9d23ec
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-25 19:39:41 +0300

    @Repository (Jpa Hibernate)
    extends JpaRepository
    extends CrudRepository

---
+++ src/main/java/com/muhammetcakir/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.muhammetcakir.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//JpaRepository>CrudRepository
+@Repository
+public interface EmployeeRepository extends CrudRepository {
+}
