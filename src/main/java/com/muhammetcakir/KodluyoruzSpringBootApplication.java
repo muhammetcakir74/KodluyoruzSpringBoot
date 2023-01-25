ace5907d51377f2b73aae278a6e8f68bd06c4815
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-25 19:12:47 +0300

    ModelMapperBean

--- src/main/java/com/muhammetcakir/KodluyoruzSpringBootApplication.java
+++ src/main/java/com/muhammetcakir/KodluyoruzSpringBootApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //Security inactive
+//@SpringBootApplication
 @SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
 )
 public class KodluyoruzSpringBootApplication {
 
