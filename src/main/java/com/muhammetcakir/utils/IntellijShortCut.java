2cf9c41da8422875209fa5613020e541e013e591
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-25 12:52:29 +0300

    IntellijShortCut

--- src/main/java/com/muhammetcakir/utils/IntellijShortCut.java
+++ src/main/java/com/muhammetcakir/utils/IntellijShortCut.java
@@ -4,5 +4,7 @@ public class IntellijShortCut {
     /*Setting ==> ctrl+alt+s
     Alt+Insert ==> constructor,toString,getter,setter
     Ctrl+/ ==> single comment
-    Ctrl+shift+/ ==> multiple comment*/
+    Ctrl+shift+/ ==> multiple comment
+    Ctrl+alt+L ==> Document Formatter
+    */
 }
