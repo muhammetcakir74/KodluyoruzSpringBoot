1a88525e355d1acea9c94c9d9916082cab0cf9df
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-25 15:45:42 +0300

    @RequestParam

--- src/main/java/com/muhammetcakir/controller/ThymeleafController.java
+++ src/main/java/com/muhammetcakir/controller/ThymeleafController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -15,8 +16,8 @@ public class ThymeleafController {
 
     //http://localhost:8080
     //optional root
-    @GetMapping({"/","index"})
-    public String index(){
+    @GetMapping({"/", "index"})
+    public String index() {
         return "index";
     }
 
@@ -91,18 +92,31 @@ public class ThymeleafController {
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //Model Object göndermek
+    //@PathVariable
     //http://localhost:8080/thymeleaf7
-    @GetMapping({"/thymeleaf7","/thymeleaf7/{id}"})
+    //http://localhost:8080/thymeleaf7/5
+    @GetMapping({"/thymeleaf7", "/thymeleaf7/{id}"})
     public String getThymeleaf7Model(Model model, @PathVariable(name = "id", required = false) Long id) {
-        if (id!=null){
-            model.addAttribute("key_model1", "id: "+id);
-        }
-        else{
+        if (id != null) {
+            model.addAttribute("key_model1", "id: " + id);
+        } else {
             model.addAttribute("key_model1", "id bulunamadı");
         }
 
         return "thymeleaf7";
+    }
+
+    //@RequestParam
+    //http://localhost:8080/thymeleaf8/id=4
+    @GetMapping("/thymeleaf8")
+    public String getThymeleaf8Model(Model model, @RequestParam(name = "id", required = false, defaultValue = "0") Long id, @RequestParam(name = "name") String adi
+    ) {
+        if (id != null) {
+            model.addAttribute("key_model1", "id: " + id + " Adı: " + adi);
+        } else {
+            model.addAttribute("key_model1", "id bulunamadı");
+        }
+        return "thymeleaf8";
     }
 
 }
