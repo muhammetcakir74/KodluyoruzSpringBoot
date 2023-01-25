b8b8686ca8726706bbeedcd2674a6a978c312cf7
ÇAKIR <AKIR@DESKTOP-VC0J0FD>
2023-01-26 00:23:31 +0300

    EmployeeServicesImpl (@Service)

---
+++ src/main/java/com/muhammetcakir/business/services/impl/EmployeeServiceImpl.java
@@ -0,0 +1,106 @@
+package com.muhammetcakir.business.services.impl;
+
+import com.muhammetcakir.business.dto.EmployeeDto;
+import com.muhammetcakir.business.services.EmployeeServices;
+import com.muhammetcakir.data.entity.EmployeeEntity;
+import com.muhammetcakir.data.repository.EmployeeRepository;
+import com.muhammetcakir.exception.ResourceNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeServices {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    //LIST
+    //http://localhost:8080/api/v1/employees
+    @GetMapping("/employees")
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<EmployeeDto> listDto = new ArrayList<>();
+        Iterable<EmployeeEntity> entityList = employeeRepository.findAll();
+        for(EmployeeEntity entity:entityList){
+            EmployeeDto employeeDto = EntityToDto(entity);
+            listDto.add(employeeDto);
+        }
+        return listDto;
+    }
+
+    //SAVE
+    //http://localhost:8080/api/v1/employees
+    @PostMapping("/employees")
+    @Override
+    public EmployeeDto createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeEntity employeeEntity = DtoToEntity(employeeDto);
+        employeeRepository.save(employeeEntity);
+        return employeeDto;
+    }
+
+    //LIST
+    //http://localhost:8080/api/v1/employees/1
+    @GetMapping("/employees/{id}")
+    @Override
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id) throws Throwable {
+        EmployeeEntity employee = (EmployeeEntity) employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee not exist with id "+id));
+        EmployeeDto employeeDto = EntityToDto(employee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+    //UPDATE
+    //http://localhost:8080/api/v1/employees/1
+    @PutMapping("/employees/{id}")
+    @Override
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDetails) throws Throwable {
+        EmployeeEntity employeeEntity = DtoToEntity(employeeDetails);//ModelMapper
+        EmployeeEntity employee = (EmployeeEntity) employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Employee not exist"));
+
+        employee.setFirstName(employeeEntity.getFirstName());
+        employee.setLastName(employeeEntity.getLastName());
+        employee.setEmailId(employeeEntity.getEmailId());
+
+        EmployeeEntity updatedEmployee = (EmployeeEntity) employeeRepository.save(employee);
+        EmployeeDto employeeDto = EntityToDto(updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+
+    //DELETE
+    //http://localhost:8080/api/v1/employees/1
+    @DeleteMapping("/employees/{id}")
+    @Override
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) throws Throwable {
+        EmployeeEntity employee = (EmployeeEntity) employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id "+id));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Model Mapper
+
+    //Entity ==> DTO
+    @Override
+    public EmployeeDto EntityToDto(EmployeeEntity employeeEntity) {
+        EmployeeDto employeeDto = modelMapper.map(employeeEntity,EmployeeDto.class);
+        return employeeDto;
+    }
+
+    //DTO ==> Entity
+    @Override
+    public EmployeeEntity DtoToEntity(EmployeeDto employeeDto) {
+        EmployeeEntity employeeEntity = modelMapper.map(employeeDto,EmployeeEntity.class);
+        return employeeEntity;
+    }
+}
